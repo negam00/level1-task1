cc1b4109e77288a3c1e86a6993e779952c489759
negam00 <negam00@github.com>
2020-02-12 16:07:17 +0100

    Compares dices now

--- app/src/main/java/com/example/level1_task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/level1_task1/HigherLowerActivity.kt
@@ -2,11 +2,88 @@ package com.example.level1_task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_higher_lower.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
+        initViews()
+
+        lowerBtn.setOnClickListener { onLowerClick() }
+        equalsBtn.setOnClickListener { onEqualClick() }
+        higherBtn.setOnClickListener { onHigherClick() }
+    }
+
+    private fun onHigherClick() {
+        rollDice()
+        if (currentThrow > lastThrow) onAnswerCorrect() else onAnswerIncorrect()
+
+    }
+
+    private fun onEqualClick() {
+        rollDice()
+        if (currentThrow == lastThrow) onAnswerCorrect() else onAnswerIncorrect()
+
+    }
+
+    private fun onLowerClick() {
+        rollDice()
+        if (currentThrow < lastThrow) onAnswerCorrect() else onAnswerIncorrect()
+
+    }
+
+    /**
+     * Displays a successful Toast message.
+     */
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Displays a incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this@MainActivity, getString(R.string.incorrect), Toast.LENGTH_SHORT).show()
+    }
+
+
+    /**
+     * Set the initial (UI) state of the game.
+     */
+    private fun initViews() {
+
+        updateUI()
+    }
+
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+
+    /**
+     * Update the last throw text and the dice image resource drawable with the current throw.
+     */
+    private fun updateUI() {
+        tvLastThrow.text = getString(R.string.last_throw, lastThrow)
+
+        when(currentThrow) {
+            1 -> ivDice.setImageResource(R.drawable.dice1)
+            2 -> ivDice.setImageResource(R.drawable.dice2)
+            3 -> ivDice.setImageResource(R.drawable.dice3)
+            4 -> ivDice.setImageResource(R.drawable.dice4)
+            5 -> ivDice.setImageResource(R.drawable.dice5)
+            6 -> ivDice.setImageResource(R.drawable.dice6)
+        }
     }
 }
+
